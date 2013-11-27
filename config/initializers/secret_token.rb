6cee60c0fb197a05b4a3206d50ca3f4d66c03a90
Abhishek <abhishekbose87@gmail.com>
2013-11-27 19:36:45 +0530

    Create home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalWebsite::Application.config.secret_key_base = '5510ba2cc67bdc6fab2def7bd09125666240d022e8c183bd9d64af5eb1def615ab9b50e740f5f681c6d95c522f3d83fcf89ee45eeae41c11c0d89ad8e12489f2'
